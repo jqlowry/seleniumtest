462a1d2872501407c5e1fb1673159f18f1572345
jqlowry <Justin.Lowry@mtsi-va.com>
2018-11-06 10:59:42 -0700

    Added Chrome

--- src/test/java/Selenium2ExampleTest.java
+++ src/test/java/Selenium2ExampleTest.java
@@ -2,6 +2,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -53,5 +54,43 @@ public class Selenium2ExampleTest {
 
         //Close the browser
         driver.close();
+
+
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.out.println(System.getProperty("webdriver.chrome.driver"));
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+        //WebDriver driver = new HtmlUnitDriver();
+        // And now use this to visit Google
+        chromeDriver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement chromeElement = chromeDriver.findElement(By.name("q"));
+
+        // Enter something to search for
+        chromeElement.sendKeys("Juice!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        chromeElement.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + chromeDriver.getTitle());
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(chromeDriver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("juice!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + chromeDriver.getTitle());
+
+        //Close the browser
+        chromeDriver.close();
     }
 }
